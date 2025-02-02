155891b44da21af7b3e1385b46332b4677b05dfa
Song Jimin <songwlals050328@gmail.com>
2025-02-03 01:55:41 +0900

    쿠키 파일 수정

--- cogs/music.py
+++ cogs/music.py
@@ -33,6 +33,7 @@ class Music(commands.Cog):
             'quiet': True,
             'no_warnings': True,
             'default_search': 'ytsearch1',
+            'cookiefile': '/home/ubuntu/komonMusicBot/cookies.txt',
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
             }
